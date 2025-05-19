2e0724d2f89a968889fb53e41c358b96dbaf3b16
jiyeon0926 <yeoni9094@naver.com>
2025-05-19 15:40:48 +0900

    feat: WebConfig

---
+++ src/main/java/com/example/demo/global/config/WebConfig.java
@@ -0,0 +1,44 @@
+package com.example.demo.global.config;
+
+import com.example.demo.global.common.enums.UserRole;
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebConfig {
+
+    private static final String[] WHITE_LIST = {"/users/signup", "/admins/signup", "/auth/login", "auth/refresh"};
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(WHITE_LIST).permitAll()
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ERROR).permitAll()
+                                .requestMatchers("/admins/**").hasRole(UserRole.ADMIN.name())
+                                .requestMatchers("/users/**").hasRole(UserRole.USER.name())
+                                .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        String hierarchy = "ROLE_ADMIN > ROLE_USER";
+
+        return RoleHierarchyImpl.fromHierarchy(hierarchy);
+    }
+}
